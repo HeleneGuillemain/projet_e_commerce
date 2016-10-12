721441570b3420552c51ea744045b769a9881adc
KilianLamiral <kilian.lamiral@gmail.com>
2016-10-12 19:14:37 +0200

    Modification de MoyenPaiementController avec Actions différentes selon le paiement choisi et vues associées

--- Fil_rouge_evente/Controllers/MoyenPaiementController.cs
+++ Fil_rouge_evente/Controllers/MoyenPaiementController.cs
@@ -29,7 +29,143 @@ namespace Fil_rouge_evente.Controllers
             }
         }
 
-        
-        
+        [HttpPost]
+        public ActionResult AjouterMoyenPaiement(int typePaiement)
+        {
+            if (ModelState.IsValid)
+            {
+                switch (typePaiement)
+                {
+                    case 0:
+                        return RedirectToAction("AjouterCarteBancaire");
+                    case 1:
+                        return RedirectToAction("AjouterCheque");
+                    case 2:
+                        return RedirectToAction("AjouterFacture");
+                    case 3:
+                        return RedirectToAction("AjouterVirement");
+                    default:
+                        return View();
+
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult AjouterCarteBancaire()
+        {
+            if (Convert.ToInt32(Session["RoleId"]) == 1)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Connexion", "Client");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AjouterCarteBancaire(CarteBancaire cb)
+        {
+            if (ModelState.IsValid)
+            {
+                cb.UtilisateurId = Convert.ToInt32(Session["ClientId"]);
+                idao.ajouterCarteBancaire(cb);
+                return RedirectToAction("LoggedIn", "Client");
+            }
+            else
+            {
+                return View(cb);
+            }
+
+        }
+
+        public ActionResult AjouterCheque()
+        {
+            if (Convert.ToInt32(Session["RoleId"]) == 1)
+            {
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Connexion", "Client");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AjouterCheque(Cheque c)
+        {
+            if (ModelState.IsValid)
+            {
+                c.UtilisateurId = Convert.ToInt32(Session["ClientId"]);
+                idao.ajouterCheque(c);
+                return RedirectToAction("LoggedIn", "Client");
+            }
+            else
+            {
+                return View(c);
+            }
+
+        }
+
+        public ActionResult AjouterFacture()
+        {
+            if (Convert.ToInt32(Session["RoleId"]) == 1)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Connexion", "Client");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AjouterFacture(Facture f)
+        {
+            if (ModelState.IsValid)
+            {
+                f.UtilisateurId = Convert.ToInt32(Session["ClientId"]);
+                idao.ajouterFacture(f);
+                return RedirectToAction("LoggedIn", "Client");
+            }
+            else
+            {
+                return View(f);
+            }
+
+        }
+
+        public ActionResult AjouterVirement()
+        {
+            if (Convert.ToInt32(Session["RoleId"]) == 1)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Connexion", "Client");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AjouterVirement(Virement v)
+        {
+            if (ModelState.IsValid)
+            {
+                v.UtilisateurId = Convert.ToInt32(Session["ClientId"]);
+                idao.ajouterVirement(v);
+                return RedirectToAction("LoggedIn", "Client");
+            }
+            else
+            {
+                return View(v);
+            }
+
+        }
     }
 }
